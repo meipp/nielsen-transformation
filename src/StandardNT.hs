0712587ecb337909eb882d876a77955bbde1804e
meipp <meipp@users.noreply.github.com>
2022-10-30 22:49:41 +0100

    Move example symbols to StandardNTExample

--- src/StandardNT.hs
+++ src/StandardNT.hs
@@ -27,12 +27,3 @@ instance NielsenTransformable () where
 
     showSymbol (Left (Terminal a)) = [a]
     showSymbol (Right (Variable x _)) = [x]
-
-a :: Symbol ()
-a = Left (Terminal 'a')
-b :: Symbol ()
-b = Left (Terminal 'b')
-x :: Symbol ()
-x = Right (Variable 'x' ())
-y :: Symbol ()
-y = Right (Variable 'y' ())
---
+++ src/StandardNTExample.hs
@@ -0,0 +1,10 @@
+module StandardNTExample where
+
+a :: Symbol ()
+a = Left (Terminal 'a')
+b :: Symbol ()
+b = Left (Terminal 'b')
+x :: Symbol ()
+x = Right (Variable 'x' ())
+y :: Symbol ()
+y = Right (Variable 'y' ())
