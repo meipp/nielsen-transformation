acdaf8cc30384108a2738943873dc1c68122344b
meipp <meipp@users.noreply.github.com>
2022-10-31 00:35:31 +0100

    Add NielsenTranformation for Brzozowski regex

---
+++ src/BrzozowskiNT.hs
@@ -0,0 +1,55 @@
+{-# OPTIONS_GHC  -Wno-orphans #-}
+
+module BrzozowskiNT where
+
+import Prelude hiding (Either (..))
+import Data.Either (Either (..))
+import NielsenTransformation
+import Brzozowski (Regex (..), deriveSymbol')
+import qualified Brzozowski
+
+showRegex :: Regex -> String
+showRegex Phi = "Ø"
+showRegex Lambda = "ε"
+showRegex (Symbol a) = [a]
+showRegex (Concatenation p q) = "(" ++ showRegex p ++ "" ++ showRegex q ++ ")"
+showRegex (Iterate p) = "(" ++ showRegex p ++ ")*"
+showRegex (Not p) = "(" ++ showRegex p ++ ")'"
+showRegex (And p q) = "(" ++ showRegex p ++ "&" ++ showRegex q ++ ")"
+showRegex (Or p q) = "(" ++ showRegex p ++ "|" ++ showRegex q ++ ")"
+
+instance NielsenTransformable Regex where
+    aa (_, (_, _)) = [DeleteTerminalPrefix]
+
+    xx (Variable _ p, (_, _))
+        | satisfiable p = [DeleteVariablePrefix]
+        | otherwise     = []
+
+    xε ((x@(Variable _ p), _), ((), _))
+        | nullable p = [VariableIsEmpty Left' x (Replacement (toSymbol x) ε)]
+        | otherwise  = []
+
+    xa ((x@(Variable xc p), _), (Terminal a, _))
+        | satisfiable dap = [VariableStartsWithTerminal Left' x (Terminal a) (Replacement (toSymbol x) (Terminal a · x'))]
+        | otherwise       = []
+        where dap = deriveSymbol' a p
+              x'  = Variable xc dap
+
+    xy ((x@(Variable xc p), _), ((Variable _ q), _))
+        -- TODO
+        -- = undefined
+        = alphabet >>= helper
+        where helper a
+                -- TODO is this correct?
+                | satisfiable dap && satisfiable daq = [VariableStartsWithTerminal Left' x ta (Replacement (toSymbol x) (ta · Variable xc dap))]
+                | otherwise = []
+                where dap = deriveSymbol' a p
+                      daq = deriveSymbol' a q
+                      ta  = Terminal a
+        -- = [VariableStartsWithVariable Left' x y (Replacement (toSymbol x) (y·x)), VariableStartsWithVariable Right' y x (Replacement (toSymbol y) (x·y))]
+
+    nullable = Brzozowski.nullable
+    satisfiable p = not (Brzozowski.impossible p) && length alphabet /= 0
+
+    showSymbol (Left (Terminal a)) = [a]
+    showSymbol (Right (Variable x p)) = [x] ++ "{" ++ showRegex p ++ "}"
