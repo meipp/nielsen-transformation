e2e1f8bd7525c245e946b947c3f3db5d3af5bef1
meipp <meipp@users.noreply.github.com>
2023-01-25 19:10:56 +0100

    Migrate Handcrafted.BenchmarkSet to Definitions.Common

--- src/Handcrafted/BenchmarkSet.hs
+++ src/Handcrafted/BenchmarkSet.hs
@@ -1,38 +1,42 @@
 module Handcrafted.BenchmarkSet where
 
-import NielsenTransformation (Equation((:=:)), Variable (Variable))
-import NoRegex.Example (a, b, x, y)
-import Brzozowski.Regex
-import Brzozowski.Example (a', b')
+import NielsenTransformation (Equation((:=:)))
+import Definitions.Common (a, b, constrainX, constrainY)
+import Brzozowski.Regex (Regex(..))
 
 satNoRegex1 :: Equation ()
 satNoRegex1 = [x] :=: [a]
+    where x = constrainX ()
 
 satNoRegex2 :: Equation ()
 satNoRegex2 = [x, a] :=: [b, y]
+    where x = constrainX ()
+          y = constrainY ()
 
 unsatNoRegex1 :: Equation ()
 unsatNoRegex1 = [x, a] :=: [b, y, x]
+    where x = constrainX ()
+          y = constrainY ()
 
 satRegex1 :: Equation Regex
-satRegex1 = [x] :=: [a', a', a', b']
-    where x = Right (Variable 'x' (Concatenation (Iterate (Symbol 'a')) (Symbol 'b')))
+satRegex1 = [x] :=: [a, a, a, b]
+    where x = constrainX (Concatenation (Iterate (Symbol 'a')) (Symbol 'b'))
 
 satRegex2 :: Equation Regex
-satRegex2 = [x, b'] :=: [y, x, b']
-    where x = Right (Variable 'x' (Concatenation (Symbol 'a') (Iterate (Symbol 'b'))))
-          y = Right (Variable 'y' (Or (Symbol 'a') (Iterate (Symbol 'b'))))
+satRegex2 = [x, b] :=: [y, x, b]
+    where x = constrainX (Concatenation (Symbol 'a') (Iterate (Symbol 'b')))
+          y = constrainY (Or (Symbol 'a') (Iterate (Symbol 'b')))
 
 unsatRegex1 :: Equation Regex
-unsatRegex1 = [x] :=: [a', a', a']
-    where x = Right (Variable 'x' (Concatenation (Iterate (Symbol 'a')) (Symbol 'b')))
+unsatRegex1 = [x] :=: [a, a, a]
+    where x = constrainX (Concatenation (Iterate (Symbol 'a')) (Symbol 'b'))
 
 unsatRegex2 :: Equation Regex
-unsatRegex2 = [x, b', y] :=: [y, x]
-    where x = Right (Variable 'x' (Iterate (Or (Symbol 'a') (Symbol 'b'))))
-          y = Right (Variable 'y' (Concatenation (Iterate (Symbol 'b')) (Symbol 'a')))
+unsatRegex2 = [x, b, y] :=: [y, x]
+    where x = constrainX (Iterate (Or (Symbol 'a') (Symbol 'b')))
+          y = constrainY (Concatenation (Iterate (Symbol 'b')) (Symbol 'a'))
 
 unsatRegex3 :: Equation Regex
-unsatRegex3 = [x, b'] :=: [y, x]
-    where x = Right (Variable 'x' (Concatenation (Symbol 'a') (Iterate (Symbol 'b'))))
-          y = Right (Variable 'y' (Or (Symbol 'a') (Iterate (Symbol 'b'))))
+unsatRegex3 = [x, b] :=: [y, x]
+    where x = constrainX (Concatenation (Symbol 'a') (Iterate (Symbol 'b')))
+          y = constrainY (Or (Symbol 'a') (Iterate (Symbol 'b')))
