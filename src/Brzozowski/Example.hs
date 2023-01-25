c443e45bfc27bbaa294fc22de1bf2916d833edd4
meipp <meipp@users.noreply.github.com>
2023-01-25 19:12:23 +0100

    Remove redundant Brzozowski.Example.{a,b} NoRegex.Example.{a,b}

--- src/Brzozowski/Example.hs
+++ src/Brzozowski/Example.hs
@@ -3,10 +3,6 @@ module Brzozowski.Example where
 import NielsenTransformation
 import Brzozowski.Regex
 
-a' :: Symbol Regex
-a' = Left (Terminal 'a')
-b' :: Symbol Regex
-b' = Left (Terminal 'b')
 x' :: Symbol Regex
 x' = Right (Variable 'x' (Symbol 'b' `Or` Lambda))
 y' :: Symbol Regex
--- src/NoRegex/Example.hs
+++ src/NoRegex/Example.hs
@@ -2,10 +2,6 @@ module NoRegex.Example where
 
 import NielsenTransformation
 
-a :: Symbol ()
-a = Left (Terminal 'a')
-b :: Symbol ()
-b = Left (Terminal 'b')
 x :: Symbol ()
 x = Right (Variable 'x' ())
 y :: Symbol ()
