48452bba6881367a564a5d38e131c989ac5bac74
meipp <meipp@users.noreply.github.com>
2023-01-31 18:38:32 +0100

    Add sat check to Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,21 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+import Parser.SMTLibParser (parseSMTFile)
+import Parser.ExtractWordEquation (asserts, variables, parseAssert, constructEquation, constructVariables, renameVariables, termToSequence)
+import NielsenTransformation (Equation((:=:)), nielsen)
+import Brzozowski.NielsenTransformable ()
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    [path] <- getArgs
+    smt <- parseSMTFile path
+
+    let vs = variables smt
+    let as = asserts smt
+    let constraints = map parseAssert as
+    let vars = constructVariables vs constraints (renameVariables vs)
+    let (t1, t2) = constructEquation constraints
+    let e = termToSequence vars t1 :=: termToSequence vars t2
+
+    putStrLn (if nielsen e then "sat" else "unsat")
